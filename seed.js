3e8ac4c09bdef30884f0fe1b66a47014e7b1249d
SasboHassani <abdellatifhassani1205@gmail.com>
2023-05-14 13:11:12 +0100

    accessing to Mysql via ORM and adding HTTP request to test routes

--- seed.js
+++
@@ -1,9 +0,0 @@
-const {faker} = require('@faker-js/faker/locale/en')
-
-new Array(3).fill(null).map(_=> (
-    {
-        name: faker.name.fullName(),
-        email: faker.internet.email(),
-        tel: faker.phone.number()
-    }
-)).forEach(console.log)
